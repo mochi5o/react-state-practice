59ca69e7820c56200d62856d80865ce2e557ca2e
mochi5o <1610.baumkuchen.124@gmail.com>
2021-04-13 20:10:49 +0900

    同一コンポーネント内でstateを変化させる

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import Child from "./Child"
 import './App.css';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const newObj = { key02: { hoge: 'hoge2', fuga: 'fuga2', id: 'key02', } }
+  const [state, setState] = useState({
+    key01: { hoge: 'hoge', fuga: 'fuga', id: 'key01', },
+    key02: { hoge: 'hoge', fuga: 'fuga', id: 'key02', },
+    key03: { hoge: 'hoge', fuga: 'fuga', id: 'key03', },
+  });
+  const handleChange = (e) => {
+    setState(createNewState(state, getValue(e)))
+  }
+  const newHoge = { key02: { hoge: 'hoge2', fuga: 'fuga2', id: 'key02', } }
+
+  const createNewState = (state, newData) => {
+    return { ...state, ...newData };
+  };
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
+
+  const getValue = (e) => {
+    return JSON.parse(e.target.value)
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+      <button
+        value={JSON.stringify(newHoge)}
+        onClick={(e) => handleChange(e)}
+      >hoge</button>
+      {JSON.stringify(state)}
     </div>
   );
 }
