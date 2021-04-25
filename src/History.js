b1203637f8f333209e1e6348c25882cb08867cb4
mochi5o <1610.baumkuchen.124@gmail.com>
2021-04-25 19:32:40 +0900

    Reducerを使ってhistoryを作ってみた 入力値を使う方法が不明

--- src/History.js
+++ src/History.js
@@ -1,41 +1,41 @@
-import React, {useReducer}  from "react";
+//useReducerをimport
+import React, {useReducer} from 'react'
 
-// //counterの初期値を0に設定
-// const initialState = 0
-// //reducer関数を作成
-// //countStateとactionを渡して、新しいcountStateを返すように実装する
-// const reducerFunc = (countState, action)=> {
-// //reducer関数にincrement、increment、reset処理を書く
-// //どの処理を渡すかはactionを渡すことによって判断する
-//   switch (action){
-//     case 'increment':
-//       return countState + 1
-//     case 'decrement':
-//       return countState - 1
-//     case 'reset':
-//       return initialState
-//     default:
-//       return countState
-//   }
-// }
-const History = () => {
-// //作成したreducerFunc関数とcountStateをuseReducerに渡す
-// //useReducerはcountStateとdispatchをペアで返すので、それぞれを分割代入
-//   const [count, dispatch] = useReducer(reducerFunc, initialState)
-// //カウント数とそれぞれのactionを実行する<Button/>を設置する
-//   return (
-//     <>
-//       <h2>カウント：{count}</h2>
-//       <ButtonGroup color="primary" aria-label="outlined primary button group">
-//         <Button onClick={()=>dispatch('increment')}>increment</Button>
-//         <Button onClick={()=>dispatch('decrement')}>decrement</Button>
-//         <Button onClick={()=>dispatch('reset')}>reset</Button>
-//       </ButtonGroup>
-//     </>
-//   )
-  return(
-    <p>Hello!!</p>
+//counterの初期値を0に設定
+const initialValue = []
+//reducer関数を作成
+//historyStateとactionを渡して、新しいhistoryStateを返すように実装する
+const reducerFunc = (historyState, action)=> {
+//reducer関数にincrement、increment、reset処理を書く
+//どの処理を渡すかはactionを渡すことによって判断する
+  switch (action){
+    // case 'increment':
+    //   return historyState + 1
+    case 'add':
+      console.log(historyState)
+      return historyState.concat("add");
+    case 'reset':
+      return initialValue
+    default:
+      return historyState
+  }
+}
+const Counter = () => {
+//作成したreducerFunc関数とhistoryStateをuseReducerに渡す
+//useReducerはhistoryStateとdispatchをペアで返すので、それぞれを分割代入
+  const [history, dispatch] = useReducer(reducerFunc, initialValue)
+//カウント数とそれぞれのactionを実行する<Button/>を設置する
+  return (
+    <>
+      <div>
+        {
+          Object.values(history).map((x) => <p>{x}</p>)
+        }
+      </div>
+      <input onChange={()=>dispatch('add')} />
+      <button onClick={()=>dispatch('reset')}>reset</button>
+    </>
   )
 }
 
-export default History;
+export default Counter
